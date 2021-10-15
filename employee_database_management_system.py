b7edf996ee1f1fd28098c1e0fafc3be2debf60c8
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-14 19:13:35 -0700

    reference and first name

--- employee_database_management_system.py
+++ employee_database_management_system.py
@@ -60,7 +60,7 @@ class Employee:
         address = StringVar()
         reference = StringVar()
         city_weighting = IntVar() #extra wages employees can get for living in the city
-        mobile = StringVar()
+        phone = StringVar()
         basic_salary = StringVar()
         over_time = StringVar()
         gross_pay = StringVar()
@@ -83,16 +83,33 @@ class Employee:
         city_weighting.set(" ")
         basic_salary.set(" ")
         other_payment_due.set("0.00")
-        #=========================================widget============================================================
-        self.txt_receipt = Text(right_frame_1a, height=23, width=42, bd=10, font=('arial', 9,'bold'))
+        #=========================================Receipt============================================================
+        self.txt_receipt = Text(right_frame_1a, bg='#c7f7fa', height=23, width=42, bd=10, font=('arial', 9,'bold'))
         self.txt_receipt.grid(row=0,column=0)
-        #=========================================heading===========================================================
+        #=========================================Heading===========================================================
         self.lb_label = Label(top_frame_2, font=('arial', 10, 'bold'), padx=6, pady=2,
-        text='Reference\tFirstname\tLastname\tAddress\t\tGender\tMobile\tNI Number\tStudent Loan\tTax\tPension \ ')
-        #=============================listbox and scrollbar===============receipt===================================
+        text='Reference\tFirstname\tLastname\tAddress\t\tGender\tMobile\tNI Number\tStudent Loan\tTax\tPension \ Deductions\tNet Pay\t\tGross Pay')
+        self.lb_label.grid(row=0, column=0, columnspan=17)
         
-        #==========================================================================================================
+        #=============================Listbox and Scrollbar==================================================
+        scrollbar = Scrollbar(top_frame_2)
+        scrollbar.grid(row=1, column=1, sticky='ns')
         
+        first_employee = Listbox(top_frame_2, width=145, height=5, font=('arial', 12, 'bold'), yscrollcommand=scrollbar.set)
+        first_employee.bind('<<Listbox_Select>>')
+        first_employee.grid(row=1, column=0, padx=1, sticky='nsew')
+        scrollbar.config(command = first_employee.xview)
+        #=========================================Reference============================================================
+        self.lb_reference= Label(left_frame_1, font=('arial', 12, 'bold'), text = 'Reference', bd=7, anchor='w')
+        self.lb_reference.grid(row=0,column=0)
+        self.txt_reference= Entry(left_frame_1, font=('arial', 12, 'bold'), bg='#c7f7fa',bd=5, width=60, justify= 'left', textvariable = reference)
+        self.txt_reference.grid(row=0,column=1)
+        
+        #------------------------------------------First Name-----------------------------------------------------------
+        self.lb_first_name= Label(left_frame_1, font=('arial', 12, 'bold'), text = 'First Name', bd=7, anchor='w')
+        self.lb_first_name.grid(row=1,column=0)
+        self.txt_first_name= Entry(left_frame_1, font=('arial', 12, 'bold'), bg='#c7f7fa',bd=5, width=60, justify= 'left', textvariable = first_name)
+        self.txt_first_name.grid(row=1,column=1)
         #==========================================================================================================
         
         
@@ -100,6 +117,18 @@ if __name__ == '__main__': #main name of the system
     root = Tk()
     application = Employee(root)
     root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
